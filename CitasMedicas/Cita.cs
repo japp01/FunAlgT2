9ea15c7591aebe006a8977d92b145715aecd5785
l0quend0 <n00407782@upn.pe>
2024-11-14 08:09:56 -0500

    added class Cita

---
+++ CitasMedicas/Cita.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitasMedicas
+{
+    internal class Cita
+    {
+        public int Numero {  get; set; }
+        public Estudiante estudiantes { get; set; }
+        public string Enfermedad {  get; set; }
+        public double precio { get; set; }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
