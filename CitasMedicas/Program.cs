38ee93572db2a13c94f0863f528383671f54baec
l0quend0 <n00407782@upn.pe>
2024-11-14 09:14:27 -0500

    program terminado

--- CitasMedicas/Program.cs
+++ CitasMedicas/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CitasMedicas;
+
+Console.WriteLine("1. Crear una cita\n2. Mostrar datos de citas\n3. Modificacion masiva\n4. Salir");
+int opcion = int.Parse(Console.ReadLine());
+
+Cita[] lista = new Cita[0];
+do
+{
+    switch (opcion)
+    {
+        case 1:
+            Array.Resize(ref lista, lista.Length + 1);
+            lista[lista.Length - 1] = new Cita();
+            lista[lista.Length- 1] = Crear();
+            Console.WriteLine("\n¡Cita añadida!");
+            Console.WriteLine("Presiona cualquier tecla para volver al menu...");
+            break;
+        case 2:
+            Listar(lista);
+            Console.WriteLine("\nPresiona cualquier tecla para volver al menu...");
+            break;
+        case 3:
+            ModMasivo(ref lista);
+            Console.WriteLine("\nDatos modificados.");
+            Console.WriteLine("Presiona cualquier tecla para volver al menu...");
+            break;
+    }
+} while (opcion != 4);
